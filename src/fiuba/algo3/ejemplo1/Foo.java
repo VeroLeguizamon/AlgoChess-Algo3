9170d9d68608ae716fffa9ab348224163fbeb7dc
sherlyporras <sherfiuba@gmail.com>
2019-11-02 01:31:38 -0300

    inicio

---
+++ src/fiuba/algo3/ejemplo1/Foo.java
@@ -0,0 +1,26 @@
+package fiuba.algo3.ejemplo1;
+
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("Foo bye");
+	}
+	
+	public String doFoo() {
+		return "Foo";
+	}
+
+	public String doBar() {
+		return "Bar";
+	}
+
+	public int doLoop() {
+				int x = 1;
+		for (int i=1; i< 3; i++) {
+			x++;
+		}
+		return x;
+	}
+
+}
