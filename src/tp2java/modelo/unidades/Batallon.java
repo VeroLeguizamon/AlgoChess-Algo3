3f82a220ab41ea2b28365079ca03409cf5066698
veroleguizamon <veronicaleguizamon@outlook.es>
2019-11-26 00:59:19 -0300

    Cambio de nombres de variables en batallon.

--- src/tp2java/modelo/unidades/Batallon.java
+++ src/tp2java/modelo/unidades/Batallon.java
@@ -16,58 +16,58 @@ public class Batallon {
 	}
 	
 	public boolean existeBatallon(Unidad unidadCentral) {
-		
-		return(this.obtenerBatallon(unidadCentral).size()==3);
-		
+		return(this.obtenerBatallon(unidadCentral).size()==3);	
 	}
+	
 	public ArrayList<Unidad> obtenerBatallon(Unidad unidadCentral) {
 		ArrayList<Unidad> batallonAux= new ArrayList<Unidad>();
-		
+		ArrayList<Unidad> adyacentes = unidadCentral.getTablero().unidadesCercanas(unidadCentral, 1);
+
 		if (unidadCentral.sePuedeUnirAlBatallon()) {
-			ArrayList<Unidad>listAdyacentes=new ArrayList<Unidad>(unidadCentral.getTablero().unidadesCercanas(unidadCentral,1));
 			batallonAux.add(unidadCentral);
-			for(Unidad unidadAdyacente:listAdyacentes) {
-				if(unidadAdyacente.sePuedeUnirAlBatallon()&(batallonAux.size()<=2)) {
+			for(Unidad unidadAdyacente: adyacentes) {
+				if(unidadAdyacente.sePuedeUnirAlBatallon() & (batallonAux.size()<=2) ) {
 					batallonAux.add(unidadAdyacente);
 				}
 			}
 		}
 		this.batallon=batallonAux;
 		return(batallonAux);
-		
 	}
 	
 	public void mover(Direccion direccion){
-		Queue<Unidad> soldadosAMover = this.formarPila();
+		Queue<Unidad> unidadesAMover = this.formarPila();
 		
-		while(!soldadosAMover.isEmpty()) {
-			Unidad soldado = soldadosAMover.poll();
-			Coordenada coordenada = direccion.calcularCoordenada(soldado.getUbicacion());
+		while(!unidadesAMover.isEmpty()) {
+			Unidad unidad = unidadesAMover.poll();
+			Coordenada coordenada = direccion.calcularCoordenada(unidad.getUbicacion());
 			
 			if(!this.coincideCoordenada(coordenada, movidos)) {
 				if(!this.coincideCoordenada(coordenada, batallon)) {
-					((UnidadMovible) soldado).mover(direccion);
-					this.movidos.add(soldado);
+					((UnidadMovible) unidad).mover(direccion);
+					this.movidos.add(unidad);
 				} else {
-					soldadosAMover.add(soldado);
+					unidadesAMover.add(unidad);
 				}
 			} else {
-				this.movidos.add(soldado);
+				this.movidos.add(unidad);
 			}
 		}
 	}
+	
 	private Queue<Unidad> formarPila(){
-		Queue<Unidad> soldados = new LinkedList<>();
+		Queue<Unidad> unidades = new LinkedList<>();
 		
-		for(Unidad soldado : this.batallon) {
-			soldados.add(soldado);
+		for(Unidad unidad : this.batallon) {
+			unidades.add(unidad);
 		}
-		return soldados;
+		return unidades;
 	}
+	
 	private boolean coincideCoordenada(Coordenada coordenada,ArrayList<Unidad>lista) {
 		if(!lista.isEmpty()) {
-			for(Unidad soldado : lista) {
-				if(soldado.tieneMismaUbicacion(coordenada)) return true;
+			for(Unidad unidad : lista) {
+				if(unidad.tieneMismaUbicacion(coordenada)) return true;
 			}
 		}
 		return false;
